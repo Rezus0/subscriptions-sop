31a34eb74304976ccca7a8050362d265e3af805d
Rezus <github.rzs@yandex.ru>
2024-09-21 20:03:29 +0300

    init

---
+++ src/main/java/com/example/subscriptions_sop/SubscriptionsSopApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscriptions_sop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionsSopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionsSopApplication.class, args);
+	}
+
+}
